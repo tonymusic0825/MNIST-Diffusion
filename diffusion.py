05361e6a5227359dc824dea0794a1b8c207be5a2
tonymusic0825 <tonymusic0825@gmail.com>
2026-09-03 21:56:47 +0900

    Added diffusion.py
    - Implements the noise scheduler

---
+++ diffusion.py
@@ -0,0 +1,21 @@
+import torch
+
+class NoiseSchedule():
+
+    def __init__(self, T=1000, beta_s=1e-4, beta_e=0.02, device="cuda"):
+        self.T = T
+        self.beta = torch.linspace(beta_s, beta_e, self.T, device=self.device) 
+        self.alpha = 1.0 - self.beta
+        self.alpha_cumprod = torch.cumprod(self.alpha, dim=0)
+
+    def forward(self, x, t):
+        noise = torch.randn_like(x)
+
+        acp = self.alpha_cumprod[t].view(-1, 1, 1, 1)
+        acp_sqrt = torch.sqrt(acp)
+        acp_sqrt2 = torch.sqrt(1 - acp)
+
+        final = (acp_sqrt*x) + (acp_sqrt2*noise)
+
+        return final, noise
+
