bb87ee8971c7514d95b4553f22873d4134371046
tonymusic0825 <tonymusic0825@gmail.com>
2026-09-06 00:28:57 +0900

    Modified terminal_display to be modular for linux and windows

--- utils.py
+++ utils.py
@@ -1,9 +1,29 @@
 import matplotlib.pyplot as plt
+import platform
+from PIL import Image
+import io
+import subprocess
+
+system = platform.system()
 
 def terminal_display(image_tensor):
 
-    img = image_tensor[0, 0].cpu().numpy()
-    plt.imshow(img, cmap="gray")
-    plt.axis("off")
 
-    plt.show()
+    if system == "Windows":
+        img = image_tensor[0, 0].cpu().numpy()
+        plt.imshow(img, cmap="gray")
+        plt.axis("off")
+
+        plt.show()  
+
+    elif system == "Linux":
+        img = image_tensor[0, 0].detach().cpu()
+        img = ((img + 1) * 127.5).clamp(0, 255).byte()
+        img = Image.fromarray(img.numpy()).resize((280, 280))  
+
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        subprocess.run(
+            ["chafa", "-f", "sixels"],
+            input=buffer.getvalue()
+        )
